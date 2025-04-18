41275a1888059a0862d89743c844efcf292eb154
xqrto <janfelixlindner@gmail.com>
2025-04-18 13:06:10 +0200

    Create GITInstall.bat

---
+++ GITInstall.bat
@@ -0,0 +1,149 @@
+::▒██   ██▒  █████   ██▀███  ▄▄▄█████▓ ▒█████  
+::▒▒ █ █ ▒░▒██▓  ██▒▓██ ▒ ██▒▓  ██▒ ▓▒▒██▒  ██▒
+::░░  █   ░▒██▒  ██░▓██ ░▄█ ▒▒ ▓██░ ▒░▒██░  ██▒
+:: ░ █ █ ▒ ░██  █▀ ░▒██▀▀█▄  ░ ▓██▓ ░ ▒██   ██░
+::▒██▒ ▒██▒░▒███▒█▄ ░██▓ ▒██▒  ▒██▒ ░ ░ ████▓▒░
+::▒▒ ░ ░▓ ░░░ ▒▒░ ▒ ░ ▒▓ ░▒▓░  ▒ ░░   ░ ▒░▒░▒░ 
+::░░   ░▒ ░ ░ ▒░  ░   ░▒ ░ ▒░    ░      ░ ▒ ▒░ 
+:: ░    ░     ░   ░   ░░   ░   ░      ░ ░ ░ ▒  
+:: ░    ░      ░       ░                  ░ ░  
+::                                                              xqrto Git installer
+
+
+
+::Copyright (c) 2025 [Your Name or Nickname]
+
+::This script ("GITinstall.bat") is provided under the following conditions:
+
+::1. **Usage and Integration**
+::   - You are allowed to use this script in your own projects and scripts.
+::   - You may insert your own scripts or commands only at clearly marked sections (e.g., comments like: :: INSERT YOUR CODE HERE).
+
+::2. **No Modification**
+::   - You are **not allowed** to modify any other part of the script.
+::   - All original comments, structure, and functions must remain **unchanged**.
+
+::3. **Redistribution**
+::   - If you distribute this script (standalone or as part of your own script), this license must be included and clearly visible.
+
+::4. **No Warranty**
+::   - The author provides this script "as is" and takes no responsibility for any damage or data loss resulting from its use.
+
+::By using this script, you agree to these terms.
+
+
+
+@%syWmTZ䯃▲ㄬ%e%OsFf%c%썇%h%h縺J縺%o%듃D㈱WC噁So% %TSㄬEy眥Y▲Wu%o%ㄬ┥A%f%噁Xy썇Ym썇w%f%噁Q噁眥% %㜲Gb%
+s%W썇縺Lㄬjㄬ%e%CQY▲mOhrs縺%t%rNYlcf듃% %f썇EㄬHhLㄬ%"%rqlS%x%aXSoWA縺S썇%q%㜲lPa㜲듃%r%Yr䯃썇aㄬe%=%㈱%C%噁Aㄬ縺B縺%a%䯃眥%l%z縺vDㄬW縺%l%㈱zRlI% %H%"%u% %zFZ▲眥%
+@%iw%e%K%c%xPq%h%gA%o%䯃ㄬii眥HLzN% %WUA%o%ㄬ┥▲%f%Rmy%f%J▲CBw縺TB%
+i%s眥xJo%f%ㄬSXL% %BDㄬwㄬz%e%dDs▲듃x%x%Zsp%i%M縺g%s%썇Jt%t%X䯃R% %Npo썇縺KN縺Hq%%cd%\%IK㜲縺L㜲t%G%kXㄬ썇듃%I%▲듃vcDz㈱Etx%P%썇ㄬ%.%縺%v%wQgIㄬ縺mM%a%v䯃eGㄬuㄬI%r%F% %㜲LIzL㈱縺縺L%G%zㄬ%o%QftOPJ듃┥%t%䯃▲Ly䯃h䯃%o%k% %噁%G%썇䯃%e%縺e㜲眥%t%i㜲yHf縺䯃䯃o%
+P%cr噁ZsG䯃tP%o%jr㈱ㄬWUtAC㈱%w%H眥H㜲kpf%e%iDK䯃A%r%YGㄬ縺眥Mjo▲%s%wDy%h%AQX%e%썇眥ㄬ㈱縺Lg%l%a┥GTQCN썇%l%hdB㜲j% %듃iSRㄬKGe眥%W%ㄬ듃hZNㄬ%r%Q噁㈱Jn▲%i%n%t%lgslcy%e%iPPGs▲G眥Y%-%h㜲듃fE%H%䯃眥縺er%o%ㄬTㄬwDJ%s%z%t%V㜲e㜲썇L썇xㄬI% %EpHF眥縺UAaz%'%N縺䯃듃JFC%N%썇I%O%㈱썇C㜲G% %skR㈱mEf%G%pn%I%ㄬShkH%P%n噁縺┥o%.%s%v%x噁眥%a%㜲B%r%r眥F噁Hㄬ% %rpc噁D㈱%f%PM眥%i%O%l%u듃噁K%e%UQ㈱㜲S眥┥g% %s縺Bn䯃┥▲Zi㜲%i%듃ll眥MY▲縺%n%cf眥R縺썇y% %dTYH㜲zS┥Z%%cd%'%ZI縺n%
+g%L㈱┥▲TuWKm㈱%o%Rㄬ㜲WBH듃縺%t%hYHQwNW%o%zㄬvXFKp% %ㄬㄬ%e%썇C▲%n%ㄬC噁a썇xQ%d%Z듃%
+[%B┥䯃┥p縺C眥縺%
+ %㜲Iq듃% %ss縺V▲% %䯃縺M▲ht% %ㄬUY%{%䯃㜲%
+        :start
+        ::Your Script Before The install---------------------------------------------------------------
+
+
+
+
+        call :update
+ %iNvo썇䯃uqgT% %QXWeI▲R% %j% %q▲ㄬ㜲v% %縺qㄬ眥㈱n縺㜲% %眥縺% %Y㈱e▲㈱% %jgEㄬDW%g%hyr▲d%o%썇w%t%噁MM%o%縺㜲ㄬiN% %qa%e%zo%n%F썇X썇Q썇%d%j眥F㜲ㄬ%
+ %썇듃G썇듃aCU% %㜲% %▲z┥D㜲KJG% %噁㈱眥EㄬJfHqy%}%ㄬ%
+ %┥ZQ▲jUI㜲N% %㈱% %㈱縺XWIaZV縺D% %▲oz썇Et%{%bnaf㜲j%
+        :style
+ %vT┥ae썇V㈱% %縺Z㜲┥Jxc噁┥% %眥% %oeD┥眥眥▲% %uC噁㈱% %d┥IYtw썇N% %㈱縺p% %PPigsz噁%@%S▲V%e%Ty┥縺a%c%ㄬ眥䯃듃眥Z썇wㄬ%h%眥썇MAT%o%o% %ㄬ眥P%o%rfㄬ%f%Pyd㈱㜲R㜲Kb眥%f%ㄬDMr%
+ %ㄬnㄬfXi┥썇縺% %▲fOjC듃E眥o% %▲lc縺▲Vv噁% %ㄬu縺㜲dH眥䯃HO% %vNㄬ% %y㈱% %縺듃䯃E縺㈱% %c%t%듃眥EKig眥㜲┥%i%縺眥s%t%oOm㜲DO縺噁㈱G%l%㈱縺Xl%e%jㄬSdㄬlG썇% %ZAr縺縺m噁ㄬ%%title%
+ %sOPR% %縺t㈱N䯃N眥ㄬ▲縺% %KY% %o㜲b썇% %䯃N噁gJ噁噁▲g% %EjsaJAxZ眥i% %噁% %X䯃L眥my%e%c噁blㄬTㄬg㈱%x%iwV縺XlkJI▲%i%ㄬrWㄬp썇gf%t%K眥hb% %Jwv噁d䯃OcT%/%㜲噁Ei듃縺v썇%b%e㈱%
+ %G% %bxv噁XC縺r% %wO% %N噁FMt%}%┥┥%
+]%ㄬ▲䯃%
+:update
+set "URL=https://raw.githubusercontent.com/%GITUSER%/%repo%/main/%filename%"
+s%P썇眥%e%eru▲┥%t%tQ縺㜲uㄬw┥噁% %┥縺㈱縺L%o%W%u%噁YkZ썇GERLU%t%hl썇䯃噁mn┥%=%EQH眥%%outfile%
+c%듃썇t縺GL%u%▲e%r%WG%l%tㄬ眥bH% %kQ眥NJ縺%-%y縺眥%-%h┥b㈱A%h%Ui縺e듃%e%sWK듃Ndo噁%a%眥縺Kㄬ䯃%d%ZvㄬUyE┥縺% %ㄬw썇眥I%-%zt%-%VuWYfC%s%SㄬU眥%i%Gq眥v眥y%l%듃ㄬPJ眥듃pyp%e%眥%n%EUkSR䯃%t%yDㄬp% %rFN%-%z썇%-%N%f%SS噁%a%K縺썇ㄬ縺agH%i%JEㄬEM%l%썇% %縺%"%N縺JdA썇Oㄬ%%URL%"%zJL㜲Cp% %噁CfFzㄬjCz噁%>%dkj▲H%n%眥┥J%u%Mx썇lUJP%l%眥% %w%2%h%>%fㄬ듃㜲G縺Eq%&%縺䯃%1%縺q▲썇%
+i%o眥k┥%f%B▲縺Lf眥% %K%%errorlevel%=%v%=%GhN%0%E㈱T듃% %W▲LCwfSJza%(%듃眥縺썇rg%
+ %S% %噁ㄬV縺縺썇썇듃Sw% %T縺a㜲▲FGk% %V噁KYkym┥%c%X▲G%u%ㄬtW眥Dj%r%d眥ZxpV%l%P▲㈱縺hYuy썇㈱% %h%-%jl縺眥%L%Zb㜲Kㄬㄬ썇mE% %噁噁眥S썇x%%URL% %縺s䯃%-%bJㄬDhEXTP眥%o%眥縺p% %VNㄬTplT썇%%out%
+)%썇kiOBM% %Xe%e%ㄬIVv%l%wO%s%GlAD%e%Z% %썇噁CfGsBgI%(%眥縺roCprF%
+ %bppFjGCvc% %ㄬUaa㈱▲d┥% %Q% %p%g%xFSId%o%ia듃aGV䯃ㄬv縺%t%썇j듃D듃P┥y眥g%o%䯃㈱aEKj眥% %眥LbD썇YUㄬu%e%縺Ph䯃coR䯃䯃%r%Lnb듃bRb%r%ㄬjO듃䯃眥Kh%
+)%X▲眥ㄬL%
+e%brms▲듃%x%ㄬ%i%slQyM┥▲pea%t%ROYPuㄬ眥wㄬ% %b썇L縺%/%x眥pi▲噁c썇z䯃%b%W┥縺Y䯃CK眥┥㜲%
+
+:err
+P%眥Uc㈱䯃VO眥%o%┥Xi噁SObhI噁%w%ㄬPruFm䯃噁r縺%e%㈱f듃縺噁woVj%r%噁縺nG듃zfU%s%b㈱U䯃SyI%h%䯃znm眥%e%㜲YjgP%l%ㄬaRㄬC㈱縺眥yd%l%c㜲Qvㄬ% %噁Sf縺ku%W%oMWbI썇E%r%ㄬAD縺┥䯃%i%M%t%mV㜲I%e%Gsrm%-%Nn眥噁Sd▲듃%H%obc%o%D噁듃䯃ㄬE%s%㜲kmVf縺썇듃K%t%v縺iS% %kRz%'%C㜲▲ijock%D%n%o%r썇FvAC%w%眥䯃ㄬP%n%yR%l%G眥D㈱Bfㄬ㜲%o%眥㈱GIP┥Xxn듃%a%眥썇t眥Dzh㈱T%d%眥▲썇l% %㜲眥D%e%LYKeE썇%r%ㄬTID縺u㈱L%r%썇r眥㜲mMG%o%縺yHRag┥Sd%r%썇┥썇듃%'%ㄬVPaLㄬ% %ㄬL┥nU㈱썇▲yP%-%q┥%f%듃ㄬtOIvmi듃䯃%o%䯃f% %ㄬG㜲ZyEN%'%Fe眥%r%LViQHssG眥O%e%ug듃pel%d%JW썇Rz%'%㈱oUIX썇J썇e%;%썇Eb듃%
+e%gWIo縺Bㄬ%x%썇sRA듃ㄬ眥%i%UYw㈱縺v縺%t%oPㄬ% %PY썇uKGiㄬ%/%썇tㄬN썇b▲%b%SWety%
+
+:Get
+::title
+s%듃aMA䯃%e%㈱眥sㄬW%t%rRkeㄬ%l%Rwj縺lb䯃%o%A썇듃oPmN┥%c%o썇A䯃l%a%썇hcJㄬ%l%㜲% %噁%e%▲ㄬSZ%n%縺%a%RX㜲ㄬsBV썇%b%DJSM㜲%l%HCKoA眥%e%l㈱Ss%d%듃o썇z┥%e%G%l%b┥f썇㈱JY%a%縺噁썇▲o%y%yyMd噁D%e%a썇眥㜲H듃縺V%d%ㄬ썇hyU%e%T%x%tqv㈱mR썇B%p%EvRDN%a%┥%n%uDO▲tlT%s%lp噁TK䯃噁㜲%i%y㈱%o%眥ㄬDBn䯃S䯃Y%n%噁%
+
+s%䯃眥b%e%BP썇ㄬ썇眥sv%t%TDOKc㜲㜲䯃% %R眥xI┥I%"%uㄬ썇bgw䯃噁썇%f%v┥Kkc%o%E%u%듃mq㈱TE㜲%n%I䯃縺㈱噁aeOy%d%듃ㄬn噁Fp%=%ㄬ듃P眥N縺%0%z%"%Ku%
+s%㈱u噁ㄬ縺aUㄬ%e%sr%t%E% %眥voQ㜲%"%wLvV듃%t%縺┥oJ%i%nㄬO䯃SPX┥%t%efIG썇XNf%l%썇G%e%w%=%D썇㈱v┥眥U듃眥v%"%噁Sog%
+
+f%㜲썇hzrZ썇TH%o%VmEp縺썇㜲Mh%r%XDvvE썇眥D% %D噁BjTCW%/%眥JTiVXm%f%RC% %듃%"%nklVMo%d%G┥듃OV┥┥BZ%e%d噁X듃썇R%l%hyu眥眥bpl%i%UtdojWT%m%moSy%s%L㈱㈱XIOW眥㈱%=%CㄬA縺AD㈱%"%G% %䯃V縺uP%%%a%LCtuㄬㄬNㄬ% %kB썇%i%썇듃f썇q噁▲듃a眥%n%┥眥d縺f▲M% %hfuw┥썇rg%(%iKyo%%cd%\%KTs%G%A噁LPCv眥B%I%D썇㈱眥䯃ㄬADa%P%ㄬ䯃%.%l%v%▲┥YgI썇%a%WYGd%r%B縺H%)%Z% %MeAY眥ifWA%d%듃썇縺aq듃%o%眥䯃䯃guWumB% %듃LOiOU眥Bl%(%h㈱縺YH眥j%
+ %cFyJj┥噁Xr噁% %xf䯃ㄬt% %wㄬ▲% %qAV眥kG썇f%i%X┥%f%Txl縺I% %썇aㄬwSAN眥眥縺%!%L%f%縺lㄬ썇pf%o%㈱噁眥ㄬAj%u%wb듃%n%O䯃䯃nY噁m▲h%d%i%!%듃vvBa썇TfCb% %▲JH썇m㈱I%e%O썇듃N▲gPㄬ%q%噁W眥k%u%oㄬUSu眥Zh% %mwEjcs%1%iㄬ% %SuzqRrLDi▲%(%B%
+ %M㈱C% %kKㄬ㈱IC썇w썇q% %縺H듃縺縺眥l䯃┥% %STTKvd% %i㈱VO썇fQZ% %fByKxFmbR% %DbJJ% %眥%s%PDPakh噁TCT%e%ㄬNㄬtN%t%ㄬ% %썇q%"%cxbEn┥%t%眥듃㜲縺ㄬ┥썇%i%I䯃Bc縺縺%t%眥U縺듃ㄬD%l%썇B噁u썇wU縺I眥%e%i%=%pl%%%a%ㄬD眥㜲듃ㄬl%"%y썇%
+ %DId% %썇EㄬN% %q썇F縺nyMMj썇% %Zcrb% %U% %㜲縺M% %eLn% %㈱%s%X%e%cP噁KXJ썇Ov%t%umvkWuL縺眥g% %縺E썇縺re▲䯃X%"%DOWs┥eㄬO%f%㈱rrimCCM%o%R%u%kj▲N%n%Wn%d%듃䯃Xr%=%IfkJ眥Sk㜲%2%hp%"%m䯃XD%
+ %ㄬ䯃Zc㜲% %C縺䯃I% %YFI眥QDG眥% %䯃q▲AJMd噁%)%R眥%
+    echo %%a | findstr /C:"[Title]" >nul
+ %㜲䯃QCeB眥% %l썇Cjisj縺% %BTa썇㜲% %眥y眥u眥xykf%i%CHaT%f%Lb% %Iqㄬ㈱orq縺┥E%!%nLxp眥썇듃S▲E%e%X썇RA%r%썇HIk%r%e%o%┥썇眥n%r%縺r䯃㈱썇oDY%l%噁ㄬ썇썇噁u縺Wㄬ%e%噁BK%v%lZKqDj▲n%e%jUGd%l%眥m썇㈱H㈱ljZ%!%썇噁m▲眥R% %NSq%=%㜲䯃썇┥Ei㈱%=%qz% %▲nyMu%0%듃SfJ眥K% %O┥썇n%(%sm眥%
+ %y䯃Ui% %z% %Zd㈱jS% %噁yㄬT% %ㄬF썇P㜲M% %縺㈱w% %썇aKN眥% %㜲ㄬYx%s%jlkO썇썇o%e%㈱VCQi%t%Uj眥Q┥g㈱G% %䯃K%"%RRBc▲N縺m%f%縺%o%S┥RYH噁wbxM%u%㜲t%n%眥噁R噁Rv㈱%d%LGbM縺▲H%=%S眥ㄬn▲ㄬ%1%mㄬK噁dYnYwX%"%U%
+ %RNMalb眥FI眥% %眥R▲% %썇a% %眥J▲Mhe%)%o䯃Qm썇WykSㄬ%
+)%▲yyE%
+s%Ox䯃㈱Id┥I%e%AG%t%縺IeKR縺f噁ㄬX% %xzny㜲NnF㈱%"%C眥b┥┥y縺X%f%㜲Y듃A㈱縺zQI%o%▲%u%t眥%n%Tt썇ㄬw썇K%d%㈱udViFD噁㈱%=%▲Kb┥䯃%0%s䯃Ymj縺%"%S㈱㜲sS㈱o썇H眥%
+s%wXH䯃㜲縺B▲n縺%e%v眥U%t%aWqo% %썇Os%"%N썇qe%G%眥T縺▲j噁C%I%TsH듃kWNyCㄬ%T%F┥眥㜲R縺%U%l%S%㈱%E%Vi썇RjㄬsQ%R%tzegi┥K%=%Hzn䯃%"%I%
+
+f%k썇㜲E썇w㈱┥ㄬ%o%듃眥F䯃W䯃%r%Pg듃ChD% %b噁s듃%/%▲n㈱㜲┥nWx▲%f%▲s% %㈱썇MY▲o䯃e%"%f眥SYTWUnS%d%Zo䯃AiUO%e%nE%l%B썇cR%i%▲mYSOCr썇%m%ㄬㄬ듃縺sC縺縺O%s%f䯃z噁lEut%=%n眥oj%"%썇LVQUkFBq% %眥㈱rxy%%%a%噁Qb% %㈱噁縺S┥frlr%i%is縺ㄬ%n%듃眥眥Yl㈱% %眥egㄬ眥┥T%(%v듃┥BVㄬM%%cd%\%┥┥ㄬO썇U썇䯃%G%㈱┥%I%ㄬKv噁qXPM▲I%P%縺E듃tla%.%U%v%v縺%a%rx듃%r%Lb%)%ㄬ眥L▲t% %OnWfs%d%aJ縺yI䯃%o%e% %L썇i縺縺K眥ㄬ%(%XcPk%
+ %┥oㄬXk┥dBe% %aQTnIq縺縺% %pHp썇㈱HB% %T眥%i%O縺lㄬkdy%f%眥䯃縺P듃% %Skk%!%ㄬ▲ㄬ%f%ㄬoTHK%o%k縺듃썇ㄬv噁Jm%u%噁㈱d%n%듃PZqBG%d%썇眥oC%!%foHr眥ㄬCUlB% %䯃EcsGN眥듃┥v%e%BgQ%q%PhF䯃듃%u%┥듃% %er縺Pwm眥oh%1%▲s㈱ㄬㄬ썇p% %p┥LOh┥nj䯃B%(%縺M噁j%
+ %ㄬRx縺썇ㄬ㈱uo% %縺% %ㄬGM眥䯃TE% %SIyㄬ㜲縺z▲T% %ecH㈱f% %ㄬjpusBy┥ㄬ썇% %y䯃X㈱Evp% %cA%s%썇ㄬEZㄬDy%e%L眥%t%㈱O眥듃㈱CUE% %B%"%㈱眥%G%iUgWJeAㄬ┥眥%I%voenQ%T%썇o%U%c%S%mBv縺jㄬf%E%oK듃QㄬqU%R%m%=%縺UmN㜲%%%a%Ri眥%"%Xq┥眥%
+ %┥縺iㄬㄬ眥C% %p▲㜲噁㈱┥KCl▲% %眥┥縺jW㈱p䯃眥縺% %L▲┥N% %nm% %眥QjrD% %kFE㜲r썇% %┥ilI┥썇Ez%s%듃QNT縺N%e%r眥縺jb%t%orft썇R┥% %眥I%"%眥眥Vn%f%듃aㄬ%o%EmSbkZ%u%bㄬ듃ㄬ듃ㄬ%n%䯃眥c㜲썇▲%d%OQc%=%AXOsV䯃㜲P%2%ye%"%H%
+ %眥㈱縺S┥gvT┥o% %KrB% %w縺縺vi썇% %噁q썇▲썇㈱d썇䯃x%)%DiEn眥┥F듃x%
+    echo %%a | findstr /C:"[GITUSER]" >nul
+ %▲Mr㜲FAJ% %㜲% %cIㄬㄬ㈱듃P┥썇% %bL%i%䯃㜲A썇yt%f%ㄬ% %Td듃t%!%gWgb眥V%e%H%r%㈱O썇眥y眥%r%眥듃縺%o%듃噁㜲듃眥PtaP%r%ㄬ썇rec썇M%l%噁듃ㄬcIJsmI%e%縺%v%jk%e%f縺%l%┥FM%!%ㄬVO縺ㄬ% %N%=%xs縺W㈱ㄬ縺䯃噁t%=%su% %썇YEa%0%ㄬ㈱T噁WhD䯃t% %j썇mtQ噁f┥N%(%㜲I썇┥썇썇%
+ %ZPCD썇m㜲EV% %噁x% %┥썇眥f% %Mh% %SI縺xR縺䯃㜲P% %▲K▲듃XhzVP䯃% %slDFo㜲QFㄬ% %tS縺噁G%s%㈱TJ䯃tㄬ%e%Ve%t%loOlff▲Y䯃% %ㄬㄬ縺O㈱X썇DE%"%N䯃縺Cv%f%o썇縺ㄬ縺SgP듃%o%gAmㄬㄬbH縺%u%qyZ%n%縺眥䯃p%d%gQ듃v%=%眥眥XHK䯃썇o縺O%1%ㄬxy듃ㄬO%"%h噁┥䯃daaZ縺㜲%
+ %┥% %Uㄬm듃FE썇% %┥┥q眥v縺u眥% %mCre%)%CuH▲Bt眥pMu%
+)%x▲%
+s%▲ljyBkv%e%㜲GU%t%v縺v䯃m䯃g% %lMs%"%眥縺gbpㄬ㜲qG%f%䯃FDEV㈱qs%o%眥%u%H%n%aif▲uSh썇%d%S썇CX썇AEk眥%=%UCBP썇l%0%yo%"%nXp眥bG%
+s%䯃%e%k噁噁O%t%fcu眥┥U% %i縺CV%"%nI眥LT䯃%r%㈱%e%l縺縺%p%x썇mg▲┥䯃Rx%o%㈱%=%PGqvb┥Lz썇p%"%縺%
+
+f%ㄬ%o%n썇ㄬQ眥%r%p縺l縺H縺┥vaX% %jwfxGmnㄬlㄬ%/%ANWcTk%f%듃wMpA% %썇Ss%"%縺j%d%䯃SLㄬ┥t%e%㜲QWPK%l%▲듃䯃SCije%i%e▲e縺듃㜲c%m%Uhq㈱g眥R眥e%s%┥q䯃qKYe듃%=%kWJ%"%┥% %眥%%%a%x듃E썇眥썇MV% %uㄬf䯃A縺㈱yㄬㄬ%i%W㜲眥RhQㄬYㄬ%n%O縺Uphu% %P眥zQiRp眥L%(%┥H%%cd%\%Nyaㄬ▲M%G%GtDTb썇%I%CVlCA㜲P┥s%P%O%.%dt㈱썇X%v%▲AtOㄬㄬOFX眥%a%▲N%r%Z▲%)%┥gTYVz縺YCa% %E%d%㈱j썇㈱ㄬ%o%jqㄬEUWX% %W썇l%(%Y▲q%
+ %썇% %nRU眥% %l% %peWgUDL%i%JDQ%f%cVxt% %rF㜲xㄬw縺%!%▲px%f%듃uㄬF%o%듃uG%u%r䯃┥%n%Y%d%yFx䯃듃%!%rQ噁Gqix% %gYTXDaAPj%e%Zd썇MqlB%q%ㄬ眥BI眥I%u%┥iUpNdW㈱㈱m% %GExv썇R%1%t㈱tT% %P䯃縺縺%(%TLV縺噁rpXfh%
+ %眥IㄬKe▲Kl% %Ag% %T% %jRjam縺sTy% %EwbVkp% %W썇% %㈱T䯃眥I▲QE㜲% %H듃X%s%┥bgYoNm%e%e%t%aizAS縺% %S眥眥縺i썇䯃%"%fNeO%r%듃썇ZQ眥X㈱%e%eㄬV縺%p%WMMJl썇bLL%o%he㈱┥w䯃眥┥Vm%=%N%%%a%P㜲H縺kw%"%P%
+ %MK縺TjGB듃噁% %qX▲b▲噁% %㜲r% %YndIjsF% %縺qP┥h% %mPbㄬ▲眥% %ㄬ썇ZdaRㄬgI% %uG䯃㈱JzU%s%眥縺NYz%e%T썇ㄬF듃%t%j% %ZJㄬO縺噁STㄬ%"%x縺r%f%T䯃XOPㄬW䯃Zl%o%M眥썇DA䯃PVV㜲%u%ㄬtㄬO%n%㜲oGZ%d%DKㄬ䯃F眥䯃%=%縺P썇眥縺S%2%kzi┥wM縺㈱yP%"%Wpuq眥l┥㜲LB%
+ %ZㄬzpQRYmㄬ噁% %f▲ㄬ듃% %XXG䯃kL㈱▲ㄬh% %┥縺%)%眥A▲%
+    echo %%a | findstr /C:"[Repository]" >nul
+ %┥䯃fK㈱l% %眥ys% %mksR縺縺縺% %R%i%眥%f%ㄬYUiV% %噁A眥bpVㄬv噁%!%WUp眥DCeR㈱%e%QO眥fmp%r%噁▲A%r%RVcztW㈱ㄬ噁%o%眥FcUBdㄬ┥%r%F썇xvKq縺%l%Ysxㄬ썇㜲%e%c┥RUw%v%d┥%e%aW▲delJV%l%眥縺ㄬG▲㜲Zx%!%㈱眥A㜲▲O噁% %iJ듃%=%lF眥㜲┥VVㄬㄬ듃%=%B% %yh縺縺m%0%zihpbkUㄬNp% %J썇%(%p眥썇%
+ %B䯃G듃眥q% %眥% %┥VC% %vZ썇縺噁ER眥L% %W% %W縺I縺┥Ot% %┥┥썇fZZU% %듃hF㜲眥㈱a縺%s%縺qㄬM眥眥%e%썇ㄬA噁gRX썇眥%t%ㄬh듃ㄬ䯃% %d%"%i眥ㄬC%f%䯃gb噁%o%眥%u%眥My縺%n%FJpRv%d%x%=%Fw縺T縺%1%噁%"%Hcl썇BsHsE%
+ %┥% %m% %Zt% %q듃縺%)%䯃㜲d썇u噁噁hVB%
+)%ㄬ%
+s%l%e%T듃SMfsONj%t%yh縺k㈱HCQd% %hJㄬDOpxwb%"%Eㄬ%f%FVUFxqRyJ┥%o%K縺V䯃t%u%듃ㄬFtCZEㄬ眥e%n%縺puiB%d%LㄬlㄬipㄬK%=%ow▲%0%zJ噁ㄬL%"%q%
+s%iuQ縺OhJK%e%TS眥P%t%眥듃Z▲M썇% %ㄬ썇bK듃%"%ㄬ썇SRViP%f%▲s썇vw%i%eesHDH縺噁rM%l%e眥g眥%e%Vcq┥y▲▲zl䯃%n%b듃┥ㄬOAkrE%a%k眥X縺㈱b%m%듃x㈱W㜲眥듃▲nS%e%q%=%AP썇mㄬ縺%"%EE┥縺噁ddㄬB%
+
+f%N㈱s%o%hjUvawtb%r%V㜲Mes┥U% %ptFnMU%/%JL䯃%f%CA% %眥縺眥u썇䯃%"%ㄬNtuIEy縺듃R%d%y썇dGFA썇g眥%e%㈱BxaH%l%bTh%i%㈱eY䯃H㜲%m%㈱䯃㈱gㄬ䯃縺%s%Gh眥JHKp썇┥%=%眥d썇B%"%ㄬ䯃kSxko㜲ㄬ% %u%%%a%듃㜲㜲EkGb% %qc듃縺F%i%y썇O%n%x▲% %ㄬ듃n噁%(%ㄬㄬv%%cd%\%rrl噁썇%G%nXhFNW%I%Ejl썇op縺YAZ%P%dCW%.%IUdㄬ眥j縺噁㜲%v%ㄬt썇vdJ縺Ml%a%J縺gPXoId%r%alxfWrc縺zj%)%Dmㄬ% %rN%d%㈱噁a듃ㄬ噁▲┥MI%o%썇縺CS% %icQE%(%d┥㈱C眥H%
+ %XNHHCF% %gN㜲In眥g眥% %zc듃G듃듃t% %M眥ㄬ%i%rx㜲A㜲%f%m眥Gud% %ZzcGFjVe%!%goQSyob썇LJ%f%XqvT縺qe듃%o%썇He%u%L噁VKv썇J%n%Kr噁䯃BO▲%d%I%!%z㈱rl% %㈱mKPo▲U㈱m듃%e%IqㄬE┥%q%㜲A%u%C썇cgK噁噁fTb% %LHY┥jㄬNH%1%C▲% %LVZz䯃QV%(%ysM%
+ %pWjㄬ噁% %yd% %pYIb噁ㄬ% %F▲썇I縺h% %k㜲C縺q㈱% %썇I% %gJ┥썇% %Oㄬ噁PIp%s%EH眥mpmA%e%q┥듃fs㈱D%t%RvNB㈱QIk㜲縺% %a%"%v眥T%f%듃y듃䯃㜲F%i%go▲LBi%l%H%e%qA眥噁fOf%n%듃rFB縺u썇J%a%썇h듃oGcp%m%q듃u%e%썇xMAn%=%xn┥U縺%%%a%T듃%"%P眥OrOSQnZ%
+ %d眥썇Q듃듃┥lik% %w듃썇% %z䯃縺% %AGBx듃vP眥M% %sU썇vs噁qF썇v% %fsu% %N噁% %ZxD%s%s▲D%e%縺㜲%t%aHntLrXz┥% %┥䯃%"%듃ey眥yzvue%f%眥u䯃眥v㜲%o%s%u%qRpStm㜲x噁%n%眥眥hT㈱%d%듃縺縺B%=%l썇o썇▲p%2%u眥n眥㜲縺E썇%"%듃DBmmr%
+ %K썇眥B䯃A% %DCkq% %slU% %Y썇z%)%ㄬ眥썇l┥b㈱Bbl%
+    echo %%a | findstr /C:"[filename]" >nul
+ %vx% %QF% %ezvhtX┥MYh% %yVcl眥㈱%i%N眥aSIryCz%f%todw㜲ayp㈱K% %Z%!%C眥眥nWSw眥T眥%e%썇Zx▲%r%YwF%r%v┥h%o%噁%r%vGYfLR듃▲썇%l%kGuJq썇wu縺A%e%vtlsB%v%duKN%e%ㄬ%l%J썇Hc▲D%!%d% %噁O썇PN%=%썇I┥眥%=%▲r┥Aw眥썇㜲V% %UB䯃yNb%0%Ho% %SvTOst듃%(%yh%
+ %㈱Y眥sR% %썇썇O% %w䯃PYbq眥% %┥c縺yaV% %眥% %㈱ㄬyK썇㜲Zyo% %G┥▲CNiaa縺e% %┥eQ䯃E%s%JHH㈱dRi듃%e%㜲㈱qOi㈱v䯃%t%x% %Hㄬ縺Ew眥d┥ah%"%x%f%wvkㄬl%o%䯃眥H듃DpHY%u%ㄬ眥ce噁lA%n%l眥Zczt䯃㜲%d%h眥Leb眥眥眥%=%XF%1%Fx┥A%"%W▲%
+ %眥KLq% %듃E㜲s縺썇hy% %噁O썇眥썇Lsy% %Nh%)%┥xㄬU%
+)%XC㈱PXo썇dD%
+s%sㄬlJoU듃t%e%uN▲O㜲E㈱䯃%t%a% %rc䯃䯃dB眥㈱%"%Wojo縺%f%썇se▲PP縺%o%n썇LrㄬBㄬ%u%x썇Z縺㈱噁G%n%j%d%aIavwhj%=%縺Usw䯃%0%fY%"%眥hFuTh%
+s%jrhos%e%d▲%t%J썇xkF䯃縺d썇F% %aaY縺ㄬ%"%y㈱㜲oge噁%o%ㄬ▲X縺tuF%u%DW%t%縺VFv眥%f%썇䯃%i%眥噁kRiO縺s%l%hg썇%e%yitoL썇a%=%H썇yJ%"%WKhufw縺%
+
+f%x㜲J㈱Al㜲眥%o%ㄬ縺D眥Bt%r%㜲c噁眥㈱㜲Y% %pnQHVM㜲썇hf%/%Iㄬc%f%㈱眥㜲wy噁Y┥縺P% %噁썇W%"%nㄬCㄬ縺v%d%NTr䯃TI噁%e%VuC眥%l%V㜲XPb%i%RS%m%V썇썇眥┥y%s%TRx噁uM%=%Q썇䯃㈱썇㜲A%"%iLpm眥▲ㄬeS% %t썇hsvYo%%%a%H% %듃噁H%i%㜲n%n%v% %Gg縺MEL▲%(%▲WV▲%%cd%\%㜲GBfwEv㜲Tj%G%M┥x㜲%I%J듃yO眥%P%噁㜲┥i縺p듃G%.%▲縺aL縺%v%X%a%ㄬoRN듃Zjx%r%eJ眥IMq%)%Ieov眥㜲omPE% %C䯃Q㜲썇YpR眥%d%l眥lRZnKC%o%䯃KH縺썇썇nr▲o% %ㄬmㄬ%(%썇Tmv%
+ %z㈱┥듃Qㄬ% %c㈱aypㄬ㈱fS% %▲BpMvㄬ% %ㄬJFj眥cW듃K%i%s㈱%f%pOQ썇┥hO% %v%!%▲E썇듃E㈱%f%RgYt㜲眥썇u┥%o%Ew┥%u%ㄬ縺㜲D%n%┥縺䯃yGaㄬ%d%A듃s%!%㈱% %B듃PCxE%e%┥gT噁yB䯃ㄬY썇%q%ssh%u%㜲Dㄬfs% %썇Pol縺ㄬG썇眥縺%1%㈱m眥縺Bu% %g㜲%(%dㄬV%
+ %a% %ㄬ썇% %L썇nSsw噁듃% %X썇q噁BBVqvl% %▲x% %MEr眥QkDJz㈱% %RLBjDc% %aFm%s%Edmb䯃듃┥%e%▲rupmW%t%s㈱zhO噁xY▲ㄬ% %Q%"%u썇e┥j%o%ㄬMPAd縺uㄬ縺噁%u%r썇J썇H┥C썇ㄬm%t%Q%f%噁Ob噁kvㄬ䯃m%i%眥n㈱uT%l%RWpSf듃%e%a眥TVMpQ縺%=%Ml썇Oo%%%a%ㄬ▲fox%"%U썇H%
+ %mZw䯃┥眥眥䯃% %S듃縺IKPㄬT% %ㄬhHㄬgT% %a眥t% %噁眥vㄬg縺X% %c縺썇j噁% %Ga┥kOGRRKs% %x%s%WxA%e%▲mR%t%듃M% %e縺FbploU%"%zD眥IR%f%FRQy%o%Q䯃%u%䯃L㜲oP%n%waf䯃䯃%d%듃LCoBㄬ썇%=%┥W%2%M㈱眥썇%"%ESN썇X%
+ %▲眥㜲AZw% %Nㄬ% %썇nS縺I% %bu噁%)%H▲R㈱┥YE%
+    echo %%a | findstr /C:"[outfilePath]" >nul
+ %眥u眥眥JuQ% %qabHS▲㜲AA썇% %眥% %T眥QN㈱vRHMx%i%HaJSxvm▲%f%yZ䯃䯃ㄬㄬ縺wma% %DLMJ眥U縺o듃%!%㜲㈱썇l썇縺┥%e%縺%r%FUㄬlU㜲Vbㄬ㈱%r%frUAgq%o%噁f▲䯃UrvfnP%r%▲dN%l%yU%e%fH%v%FuL%e%kTkyR썇w㜲m%l%䯃Ix縺㜲ㄬ噁vb㈱%!%ㄬㄬuTiecnAv% %ajuIn%=%q▲眥ZrL듃R%=%vm듃v眥% %眥┥㈱M썇%0%u㜲㈱S썇Wn% %噁Jj%(%k噁㈱썇b縺I%
+ %㜲eV% %P眥㈱GcLnㄬ% %V䯃眥K% %縺jg縺듃% %㜲䯃UqVqkS% %dINF眥% %Q縺q% %xㄬl%s%WbCw㜲FkmD%e%iPoS%t%㜲fXNS㈱e縺YZ% %眥%"%qhub噁JUㄬ듃%f%▲듃s眥iMNL%o%噁N%u%GeTUr%n%MNSOktnU%d%QIbU眥縺B䯃MB%=%a䯃C㜲썇NU縺rR%1%ZZh%"%HYㄬ%
+ %듃T듃Qr┥% %IjYY縺oK썇Xy% %C㜲縺w% %㜲M썇t썇%)%▲%
+)%sG眥縺XㄬwBr%
+Call :style
+g%듃U縺OF%o%眥BMOGQ%t%XYsl듃epe%o%㈱% %pcEnㄬ㜲%s%tJ䯃ZcZ%t%S%a%acPXBzj%r%zENejH%t%i▲mOgㄬ縺듃%
+
+
+:end
+::---------------- Script after install------------------
